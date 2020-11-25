529a2f05c261aab161c91f182cc6920574b3d7d8
Tobias Kummer <tobias.kummer@lst.team>
2020-11-25 10:30:51 +0100

    [TASK] Show palettes `dimension_settings` and `background_settings` if "lst-fluid-styled-content" is installed

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -105,4 +105,26 @@ defined('TYPO3_MODE') or die();
                         categories,
                     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
                         rowDescription';
+
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('lst_fluid_styled_content')) {
+        $GLOBALS['TCA']['tt_content']['types']['teasermanager_teaser']['showitem'] = '
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+                        --palette--;LLL:EXT:teaser_manager/Resources/Private/Language/locallang_db.xlf:teaser;teaser_type_palette,
+                    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                        --palette--;LLL:EXT:lst_fluid_styled_content/Resources/Private/Language/locallang_db.xlf:palette.background_settings;background_settings,
+                        --palette--;LLL:EXT:lst_fluid_styled_content/Resources/Private/Language/locallang_db.xlf:palette.dimension_settings;dimension_settings,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                        --palette--;;language,
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                        --palette--;;hidden,
+                        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                        categories,
+                    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                        rowDescription';
+    }
 })();
